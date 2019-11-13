f782c6c8fbe1c92b3a045d3e140508bd1a86a145
Bezaleel Ashefor <knightbenax@gmail.com>
2019-11-13 09:01:39 +0100

    Test Deployment

--- public/assets/js/worker.js
+++ public/assets/js/worker.js
@@ -1,10 +1,32 @@
 $(document).ready(function (){
-    anime.timeline().add({
-        targets: '.section-hero',
-        height: '90vh',
-        elasticity: 400,
-        delay(el, i) {
-          return i * 250;
-        }
-      })
+
+var delayInMilliseconds = 3000; //1 second
+
+setTimeout(function() {
+  //your code to be executed after 1 second
+  //runAnimations();
+  document.getElementsByClassName('zerodark-cloak-cover')[0].removeAttribute('v-cloak');
+  runAnimations()
+}, delayInMilliseconds);
+
 });
+
+
+function removeVeil(){
+
+}
+
+function runAnimations(){
+  anime.timeline().add({
+    targets: '.section-hero',
+    height: '80vh',
+    elasticity: 600,
+    opacity: [0, 1],
+    duration: 2500,
+  }).add({
+    targets: '.zerodark-section-services-container',
+    opacity: [0, 1],
+    duration: 2500,
+    easing: 'spring(1, 80, 10, 0)',
+  })
+}
