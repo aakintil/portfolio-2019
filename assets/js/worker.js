232ef0d3e0c9187a574761c5a12b5591f0da6d5b
Bezaleel Ashefor <knightbenax@gmail.com>
2019-12-03 11:44:01 +0100

    checkpoint: process animations

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -4,7 +4,6 @@ var delayInMilliseconds = 3000; //1 second
 
 setTimeout(function() {
   //your code to be executed after 1 second
-  //runAnimations();
   document.getElementsByClassName('zerodark-cloak-cover')[0].removeAttribute('v-cloak');
   runAnimations();
 }, delayInMilliseconds);
@@ -18,22 +17,54 @@ $(window).scroll(function() {
       wH = $(window).height(),
       wS = $(this).scrollTop();
   if (wS > (hT+hH-wH)){
-    console.log("balls");
     removeVeil();
     //valuesScrollAnimation.play();
   }
+
+  var chT = ($('.section-process').offset().top),
+      chH = ($('.section-process').outerHeight() - $('.section-process-image').outerHeight()) - 110,
+      cwH = $(window).height(),
+      cwS = $(this).scrollTop();
+  if (cwS > (chT+chH-cwH)){
+    removeProcessVeil()
+  }
+
 });
 
 
 var showServices = false;
+var showProcess = false;
+
+
+function removeProcessVeil(){
+  if (!showProcess){
+    anime.timeline().add({
+      targets: '.section-process',
+      opacity: [0, 1],
+      duration: 800,
+      easing: 'easeInSine'
+    }).add({
+      targets: '.section-process-image',
+      opacity: [0, 1],
+      scale: [1.2, 1],
+      delay: anime.stagger(100, {easing: 'easeOutQuad'})
+    });
+    showProcess = true;
+  }
+}
 
 function removeVeil(){
   //valuesScrollAnimation.play();
   if (!showServices){
-    anime({
+    anime.timeline().add({
       targets: '.zerodark-services-detail-container',
       opacity: [0, 1],
       easing: 'easeInSine'
+    }).add({
+      targets: '.services-detail-container',
+      opacity: [0, 1],
+      scale: [1.2, 1],
+      delay: anime.stagger(100, {easing: 'easeOutQuad'})
     });
     showServices = true;
   }
